093b93d2751ac30c741d81deb63ae64e866309a5
ameurant <alain.meurant@netvaast.be>
2020-10-19 16:22:15 +0200

    Gestion des tounées
    - ajouter une tournée
    - lister une trounée
    - modifier une tournée

--- src/mitsibox/browser/manage_rounds.py
+++ src/mitsibox/browser/manage_rounds.py
@@ -18,19 +18,48 @@ class ManageRounds(ConnexionDb):
 
     def getAllRounds(self):
         """
-        Récupères les infos de toutes les tournées
+        Récupère les infos de toutes les tournées
         """
         session = self.getConnexion()
-
         db=session.get_schema('mitsi_chuhautesenne')
         tables = db.get_tables()
-        
-        tbl_mitsidriver = db.get_collection('mitsibox_rounds')
-        recs = tbl_mitsidriver.find().execute()
-        #recs = tbl_mitsibox.select().execute()
+
+        tbl_mitsiround = db.get_collection('mitsibox_rounds')
+        recs = tbl_mitsiround.find().execute()
         myRounds = recs.fetch_all()
-        
         return myRounds
+
+    def getRoundById(self, idRound):
+        """
+        Récupère les infos d'une tournée selon son _id
+        """
+        session = self.getConnexion()
+        db=session.get_schema('mitsi_chuhautesenne')
+        tables = db.get_tables()
+
+        tbl_mitsiround = db.get_collection('mitsibox_rounds')
+        recs = tbl_mitsiround.find("_id=='%s'"%(idRound,)).execute()
+        myRound = recs.fetch_one()
+        return myRound
+
+    def getRoundsOfBox(self, idBox):
+        """
+        Récupère le nom dde la tournée à la quelle appartient une box
+        """
+        session = self.getConnexion()
+        db=session.get_schema('mitsi_chuhautesenne')
+        tables = db.get_tables()
+
+        tbl_mitsiround = db.get_collection('mitsibox_rounds')
+
+        myRound = session.sql("""select
+                                    doc->>"$.roundName"
+                                 from
+                                    mitsi_chuhautesenne.mitsibox_rounds
+                                 where
+                                    "%s" member of (doc->>"$.mitsiboxList")""" % (idBox,))
+        import pdb; pdb.set_trace()
+        return myRound
 
     def insertRound(self):
         """
@@ -42,41 +71,12 @@ class ManageRounds(ConnexionDb):
 
         fields = self.request.form
 
-
-# XXXXXXXX 
-#    UTILISER LE CODE DE FRED POUR AJOUTER UN ENSEMBLE
-#    D'ID DE BOITES DANS UN ROUND
-
-# Ajouter
-# driver_id=db.mitsibox_drivers.find("firstname='Steve'").fields('_id')
-# box_list=db.mitsibox_boxes.find().fields('_id')
-# box_list_arr=[]
-# for el in box_list.execute().fetch_all():
-#    box_list_arr.append(el.values()[0])
-# doc={"name": "du chef", "type": "daily", "box_list": box_list_arr, "start_time": "09:00:00", "estimated_end_time": "17:00:00"}
-# db.mitsibox_rounds.add(doc)
-
-
-# Lister
-# db.mitsibox_rounds.find()
-# {
-#     "_id": "00005ecb95df0000000000000016",
-#     "name": "du chef",
-#     "type": "daily",
-#     "box_list": [
-#         "00005ecb95df000000000000000c",
-#         "00005ecb95df0000000000000010",
-#         "00005ecb95df0000000000000011",
-#         "00005ecb95df0000000000000012"
-#     ],
-#     "start_time": "09:00:00",
-#     "estimated_end_time": "17:00:00"
-# }
-
-
         dico = {}
-        dico['roundtName'] = fields.get('roundtName', None)
-        dico['mitsiboxList'] = fields.get('mitsiboxList', None)
+        dico['roundName'] = fields.get('roundName', None)
+        dico['roundType'] = fields.get('roundType', None)
+        dico['roundStartTime'] = fields.get('roundStartTime', None)
+        dico['roundEstimedTime'] = fields.get('roundEstimedTime', None)
+        dico['roundMitsiboxList'] = fields.get('roundMitsiboxList', None)
 
         newRound = json.dumps(dico)
 
@@ -96,23 +96,49 @@ class ManageRounds(ConnexionDb):
         """
         session = self.getConnexion()
         db = session.get_schema('mitsibox')
-        box = db.get_collection('mitsibox_rounds')
+        round = db.get_collection('mitsibox_rounds')
 
         fields = self.request.form
+        idRound = fields.get('idRound', None) 
 
         dico = {}
-        dico['nom'] = fields.get('driverNom', None)
-        dico['prenom'] = fields.get('driverPrenom', None)
-        dico['gsm'] = fields.get('driverGsm', None)
+        dico['roundName'] = fields.get('roundName', None)
+        dico['roundType'] = fields.get('roundType', None)
+        dico['roundStartTime'] = fields.get('roundStartTime', None)
+        dico['roundEstimedTime'] = fields.get('roundEstimedTime', None)
+        dico['roundMitsiboxList'] = fields.get('roundMitsiboxList', None)
 
-        newDriver = json.dumps(dico)
+        newRound = json.dumps(dico)
 
-        # box.modify("_id='%s'" % box_id).patch(patch_json).execute()(newBoite).execute()
+        round.modify("_id='%s'" % idRound).patch(patch_json).execute()(newRound).execute()
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
-        message = u"les données du chauffeur ont été modifiées."
+        message = u"Les données de la tounrée ont été modifiées."
         ploneUtils.addPortalMessage(message, 'info')
-        url = "%s/listing-des-chauffeurs" % (portalUrl,)
+        url = "%s/listing-des-tournees" % (portalUrl,)
         self.request.response.redirect(url)
         return ''
+
+
+
+
+# XXXXXXXX 
+
+
+# Lister
+# db.mitsibox_rounds.find()
+# {
+#     "_id": "00005ecb95df0000000000000016",
+#     "name": "du chef",
+#     "type": "daily",
+#     "box_list": [
+#         "00005ecb95df000000000000000c",
+#         "00005ecb95df0000000000000010",
+#         "00005ecb95df0000000000000011",
+#         "00005ecb95df0000000000000012"
+#     ],
+#     "start_time": "09:00:00",
+#     "estimated_end_time": "17:00:00"
+# }
+
