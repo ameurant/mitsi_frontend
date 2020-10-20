7e9b0f620d6f322d649bdbf8acf5c0fdeec44e5b
ameurant <alain.meurant@netvaast.be>
2020-10-20 09:26:43 +0200

    Fixer le bug sur lacure remplacé par mitsibox

--- README.rst
+++ README.rst
@@ -1,15 +1,15 @@
 ===================
-lacure :: website
+Mitsibox :: website
 ===================
 
-Plone Product pour le gîte la cure à Jarnioux.
+Plone Product pour le projet Mitsibox.
 
 Development
 -----------
 
 ::
 
-    git clone https://github.com/ameurant/lacure.git lacure
-    cd lacure
+    git clone https://github.com/ameurant/mitsibox.git mitsibox
+    cd mitsibox
     ./bootstrap.sh
 
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 version = '5.1.4 '
 
 setup(
-    name='lacure',
+    name='mitsibox',
     version=version,
-    description="Plone product for lacure website",
+    description="Plone product for mitsibox website",
     long_description=open("README.rst").read(),
     classifiers=[
         "Framework :: Plone",
@@ -17,7 +17,7 @@ setup(
     ],
     author='Netvaast',
     author_email='alain.meurant@netvaast.be',
-    url='https://github.com/ameurant/lacure',
+    url='https://github.com/ameurant/mitsibox',
     license='GPL version 2',
     packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'': 'src'},
