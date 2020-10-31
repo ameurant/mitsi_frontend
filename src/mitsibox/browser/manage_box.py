9ed4a110cdd4bb8626074c9626bd04079fb0367a
ameurant <alain.meurant@netvaast.be>
2020-10-31 22:43:56 +0100

    Modifeir les données d’une boites - Modifier affichage des données

--- src/mitsibox/browser/manage_box.py
+++ src/mitsibox/browser/manage_box.py
@@ -22,14 +22,14 @@ class ManageBox(ConnexionDb):
         """
         session = self.getConnexion()
 
-        db=session.get_schema('mitsi_chuhautesenne')
+        db = session.get_schema('mitsi_chuhautesenne')
         tables = db.get_tables()
-        
+
         tbl_mitsibox = db.get_collection('mitsibox_boxes')
         recs = tbl_mitsibox.find().execute()
-        #recs = tbl_mitsibox.select().execute()
+        # recs = tbl_mitsibox.select().execute()
         myBoxes = recs.fetch_all()
-        
+
         return myBoxes
 
     def getOneBoxById(self, idBox):
@@ -38,13 +38,13 @@ class ManageBox(ConnexionDb):
         """
         session = self.getConnexion()
 
-        db=session.get_schema('mitsi_chuhautesenne')
+        db = session.get_schema('mitsi_chuhautesenne')
         tables = db.get_tables()
-        
+
         tbl_mitsibox = db.get_collection('mitsibox_boxes')
         recs = tbl_mitsibox.find("_id=='%s'"%(idBox,)).execute()
         myBoxe = recs.fetch_one()
-        
+
         return myBoxe
 
     def insertBox(self):
@@ -57,26 +57,25 @@ class ManageBox(ConnexionDb):
 
         fields = self.request.form
 
-        dico = {}
-        dico['lab_id'] = fields.get('laboId', None)
-        dico['name'] = fields.get('boxName', None)
-        dico['address'] = fields.get('boxAddress', None)
-        dico['cp'] = fields.get('boxCp', None)
-        dico['localite'] = fields.get('boxLocalite', None)
-        dico['lat'] = fields.get('boxLat', None)
-        dico['long'] = fields.get('boxLong', None)
-        dico['ssid'] = fields.get('boxSsidWifi', None)
-        dico['passwifi'] = fields.get('boxPassWifi', None)
+        newBox = {}
+        newBox['lab_id'] = fields.get('laboId', None)
+        newBox['name'] = fields.get('boxName', None)
+        newBox['address'] = fields.get('boxAddress', None)
+        newBox['cp'] = fields.get('boxCp', None)
+        newBox['localite'] = fields.get('boxLocalite', None)
+        newBox['lat'] = fields.get('boxLat', None)
+        newBox['long'] = fields.get('boxLong', None)
+        newBox['ssid'] = fields.get('boxSsidWifi', None)
+        newBox['passwifi'] = fields.get('boxPassWifi', None)
+        newBox['arduino'] = fields.get('boxArduino', None)
 
-        newBoite = json.dumps(dico)
-
-        box.add(newBoite).execute()
+        box.add(newBox).execute()
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
-        message = u"Ok le guide a été bien enregistrée."
+        message = u"Ok les données de la boite ont été enregistrées."
         ploneUtils.addPortalMessage(message, 'info')
-        url = "%s/listing-des-mitsibox" % (portalUrl,)
+        url = "%s/gestion-des-boites/listing-des-mitsibox" % (portalUrl,)
         self.request.response.redirect(url)
         return ''
 
@@ -85,30 +84,32 @@ class ManageBox(ConnexionDb):
         insertion d'une nouvelle boite
         """
         session = self.getConnexion()
-        db = session.get_schema('mitsibox')
+        db = session.get_schema('mitsi_chuhautesenne')
         box = db.get_collection('mitsibox_boxes')
 
         fields = self.request.form
+        idBox = fields.get('idBox', None)
 
-        dico = {}
-        dico['lab_id'] = fields.get('laboId', None)
-        dico['name'] = fields.get('boxName', None)
-        dico['address'] = fields.get('boxAddress', None)
-        dico['cp'] = fields.get('boxCp', None)
-        dico['localite'] = fields.get('boxLocalite', None)
-        dico['lat'] = fields.get('boxLat', None)
-        dico['long'] = fields.get('boxLong', None)
-        dico['ssid'] = fields.get('boxSsidWifi', None)
-        dico['passwifi'] = fields.get('boxPassWifi', None)
+        myBox = {}
+        myBox['lab_id'] = fields.get('laboId', None)
+        myBox['name'] = fields.get('boxName', None)
+        myBox['address'] = fields.get('boxAddress', None)
+        myBox['cp'] = fields.get('boxCp', None)
+        myBox['localite'] = fields.get('boxLocalite', None)
+        myBox['lat'] = fields.get('boxLat', None)
+        myBox['long'] = fields.get('boxLong', None)
+        myBox['ssid'] = fields.get('boxSsidWifi', None)
+        myBox['passwifi'] = fields.get('boxPassWifi', None)
+        myBox['arduino'] = fields.get('boxArduino', None)
 
-        newBoite = json.dumps(dico)
+        print "myBox : %s" % (myBox,)
 
-        # box.modify("_id='%s'" % box_id).patch(patch_json).execute()(newBoite).execute()
+        box.modify("_id='%s'" % idBox).patch(myBox).execute()
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
-        message = u"Ok le guide a été bien enregistrée."
+        message = u"Ok les données de cette boite ont été modifiées."
         ploneUtils.addPortalMessage(message, 'info')
-        url = "%s/listing-des-mitsibox" % (portalUrl,)
+        url = "%s/gestion-des-boites/listing-des-mitsibox" % (portalUrl,)
         self.request.response.redirect(url)
         return ''
