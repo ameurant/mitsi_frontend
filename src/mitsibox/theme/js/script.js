25284b5600e26c5e3bfedf646e55b64aa345b30e
ameurant <alain.meurant@netvaast.be>
2020-11-27 15:34:10 +0100

    Ajouter les packages mockup patern
    Configuration sur le plone
    1. aller dans resource registry
    2. dans la colonne Bindles, cliquer sur Plone
    3. un panneau s'ouvre sur la droite, dans resources, ajouter le package mockup-paterns que tu veux via la liste déroulante
    3. Save --> la bouton n'est pas en dessous mais en haut à gauche (Logique chez les gourous plone)
    4. Build le bulles plone
    
    5. Aller dans ton fichier script.js de ton thème et copier coller
    require([
        '++resource++mockup/tooltip/pattern.js'
      ], function(){});

--- src/mitsibox/theme/js/script.js
+++ src/mitsibox/theme/js/script.js
@@ -3,3 +3,10 @@ script for box project
 netvaast 2018
 ***/
 
+require([
+    '++resource++mockup/tooltip/pattern.js'
+  ], function(){});
+
+require([
+    '++resource++mockup/datatables/pattern.js'
+], function(){});
