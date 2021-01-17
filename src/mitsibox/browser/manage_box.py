075ecbe5dc2e0c81da9c2e4999432fe0d8004b5a
ameurant <alain.meurant@netvaast.be>
2021-01-17 18:26:06 +0100

    Ajouter l’ID de la box

--- src/mitsibox/browser/manage_box.py
+++ src/mitsibox/browser/manage_box.py
@@ -45,7 +45,7 @@ class ManageBox(ConnexionDb):
         filtre = "_id in {0}".format(list(i.encode() for i in idBoxList))
 
         tablesBoxes = self.getLabDbAccess('mitsibox_boxes')
-        recs = tablesBoxes.find(filtre).fields('name', 'address', 'cp', 'localite', 'lat', 'long').execute()
+        recs = tablesBoxes.find(filtre).fields('_id', 'name', 'address', 'cp', 'localite', 'lat', 'long').execute()
         allBoxes = recs.fetch_all()
         return allBoxes
 
