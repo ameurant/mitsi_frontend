848e3c91c413dadcff0056f27497697d6414a4bf
ameurant <alain.meurant@netvaast.be>
2021-01-21 10:38:06 +0100

    Insertion d’une collecte de saclots dans les deux tables
    mitsibox_collect_history
    mitsibox_collect_box_history
    
    Récupération de l’ID de lors de l’insertion dans mitsibox_collect_history pour complèter

--- src/mitsibox/browser/manage_collections.py
+++ src/mitsibox/browser/manage_collections.py
@@ -8,6 +8,7 @@ from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from interfaces import IManageCollections
 from connexion_db import ConnexionDb
+import mysql.connector
 
 
 class ManageCollections(ConnexionDb):
@@ -19,19 +20,20 @@ class ManageCollections(ConnexionDb):
 
     def getAllCollections(self):
         """
-        Récupères les collectes de saclots
+        Récupères les collectes de saclots 
+        table mitsibox_collect_history
         """
-        tableCollections = self.getLabDbAccess('mitsibox_collection')
-        recs = tableCollections.find().execute()
-        # recs = tbl_mitsibox.select().execute()
+        tableCollections = self.getLabDbAccessTable('mitsibox_collect_history')
+        recs = tableCollections.select().execute()  # cas d'une table
         myCollections = recs.fetch_all()
         return myCollections
 
     def getCollectionById(self, idCollection):
         """
-        Récupères les infos d'une collecte de saclot selon son ID
+        Récupère les infos d'une collecte de saclot selon son ID
+        table mitsibox_collect_history
         """
-        tableCollections = self.getLabDbAccess('mitsibox_collection')
+        tableCollections = self.getLabDbAccessTable('mitsibox_collect_history')
         recs = tableCollections.find("_id=='%s'" % (idCollection,)).execute()
         myCollection = recs.fetch_one()
         return myCollection
@@ -39,18 +41,40 @@ class ManageCollections(ConnexionDb):
     def insertCollection(self):
         """
         insertion d'une collecte de saclot pa' l'tchauffeu
+        table mitsibox_collect_history
         """
-        tableCollections = self.getLabDbAccessTable('mitsibox_collection')
-
         fields = self.request.form
-
         round_id = fields.get('idRound', None)
         driver_id = fields.get('idDriver', None)
+        samples_total = fields.get('totalSaclots', None)
+        collect_comment = fields.get('collectComment', None)
         box_id = fields.get('boxId', None)
-        total_samples = fields.get('totalSaclots', None)
-        samples = fields.get('samples', None)
+        collect_box_comment = fields.get('collectBoxComment', None)
+        status = 'ACTIVE'
 
-        tableCollections.insert(['round_id', 'driver_id', 'box_id', 'total_samples', 'samples']).values(round_id, driver_id, box_id, total_samples, samples).execute()
+        # ouverture de la connexion
+        session = self.getConnexion()
+        db = session.get_schema("mitsi_chuhautesenne")
+
+        # recupe des deux tables
+        tableCollectHistory = db.get_table('mitsibox_collect_history')
+        tableCollectBoxHistory = db.get_table('mitsibox_collect_box_history')
+
+        # Insertion dans la table tableCollectHistory
+        # tableCollectHistory = self.getLabDbAccessTable('mitsibox_collect_history')
+        tableCollectHistory.insert(['round_id', 'driver_id', 'box_id', 'samples_total', 'collect_comment', 'status']).values(round_id, driver_id, box_id, samples_total, collect_comment, status).execute()
+        session.commit()
+
+        # recup du dernier _id qui vient d'être inséré
+        request = session.sql("select last_insert_id()").execute()
+        collect_history_id = request.fetch_one()[0]
+        print "collect_history_id : %s" % (collect_history_id,)
+        print "box_id : %s" % (box_id,)
+
+        # Insertion dans la table tableCollectBoxHistory
+        # tableCollectBoxHistory = self.getLabDbAccessTable('mitsibox_collect_box_history')
+        tableCollectBoxHistory.insert(['collect_history_id', 'box_id', 'collect_box_comment']).values(collect_history_id, box_id, collect_box_comment).execute()
+        session.commit()
 
         portalUrl = getToolByName(self.context, 'portal_url')()
         ploneUtils = getToolByName(self.context, 'plone_utils')
@@ -63,8 +87,9 @@ class ManageCollections(ConnexionDb):
     def updateCollection(self):
         """
         update des données d'une collecte de saclots pa' l' tchauffeu
+        table mitsibox_collect_history
         """
-        tableCollections = self.getLabDbAccess('mitsibox_collection')
+        tableCollections = self.getLabDbAccessTable('mitsibox_collect_history')
 
         fields = self.request.form
         idCollection = fields.get('idCollection', None)
